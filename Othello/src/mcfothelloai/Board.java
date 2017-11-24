61e8be1b3544ad69b1e56744b5860a7fbb9cea52
vfrost <vfrost@lua.attlocal.net>
2017-11-24 18:03:54 -0500

    Revert to this if we break everything.

--- Othello/src/mcfothelloai/Board.java
+++ Othello/src/mcfothelloai/Board.java
@@ -4,214 +4,203 @@ import java.util.ArrayList;
 
 public class Board {
 
-    int[][] board;
-    
-    Board() {
-        this.board = new int[10][10];
-        for (int x = 0; x < 10; x++) {
-            for (int y = 0; y < 10; y++) {
-                if (x < 10) {
-                    if (y == 0 || y == 9) {
-                        board[x][y] = -2;
-                    }
-                } else {
-                    board[x][y] = 0;
-                }
-                if (x == 0 || x == 9) {
-                    board[x][y] = -2;
-                }
-            }
-        }//Initialize the board
+	int[][] board = new int[10][10];;
 
-        board[4][4] = 1;
-        board[5][5] = 1;
-        board[4][5] = -1;
-        board[5][4] = -1;
-    }
+	Board() {
+		// this.board = new int[10][10];
+		for (int x = 0; x < 10; x++) {
+			for (int y = 0; y < 10; y++) {
+				if (x < 10) {
+					if (y == 0 || y == 9) {
+						board[x][y] = -2;
+					}
+				} else {
+					board[x][y] = 0;
+				}
+				if (x == 0 || x == 9) {
+					board[x][y] = -2;
+				}
+			}
+		} // Initialize the board
 
-    public void BoardDraw() {
-        for (int x = 0; x < 10; x++) {
-            System.out.print("C |");
-            for (int y = 0; y < 10; y++) {
-                switch (board[x][y]) {
-                    case Game.BLACK:
-                        System.out.print("B");
-                        break;
-                    case Game.BORDER:
-                        System.out.print("X");
-                        break;
-                    case Game.EMPTY:
-                        System.out.print("0");
-                        break;
-                    case Game.WHITE:
-                        System.out.print("W");
-                        break;
-                }
-                System.out.print("|");
-            }
-            System.out.println();
-        }//Print the initial board state
-        System.out.println();
-    }//constructor 
+		board[4][4] = 1;
+		board[5][5] = 1;
+		board[4][5] = -1;
+		board[5][4] = -1;
+	}
 
-    public Board(Board board){
-        for (int r = 1; r <= 9; r++) {
-            for (int c = 1; c <= 9; c++) {
-                this.board[r][c] = board.board[r][c];
-            }
-        }
-    }//AlphaBeta constructor
-    
-    public void FindValidMoves(int color, ArrayList<Move> movelist) {
-        int r;
-        int c;
-        int i = 0;
-        int opp = -color;
-        for (r = 1; r <= 9; r++) {
-            for (c = 1; c <= 9; c++) {
-                if (board[r][c] == Game.EMPTY) {
-                    //System.out.println("found empty space " + r + " " + c);
-                    for (int dR = -1; dR <= 1; dR++) {
-                        for (int dC = -1; dC <= 1; dC++) {
-                            if (board[r + dR][c + dC] == opp) {
-                                int OppPiece = 1;//Possibly 2
-                                //System.out.println("test " + r + " " + c + " " + dR + " " + dC);
-                                //System.out.println((r+OppPiece*dR) + " " + (c+OppPiece*dC));
-                                while (board[r + OppPiece * dR][c + OppPiece * dC] == opp) {
-                                    //board[r][c] = board[r - dR][c - dC];
-                                    OppPiece++;
-                                    //System.out.println("looping");
-                                }
-                                //System.out.println((r+OppPiece*dR) + " " + (c+OppPiece*dC));
-                                if (board[r + OppPiece * dR][c + OppPiece * dC] == color) {
-                                    //System.out.println("add to valid move");
-                                    Move valid = new Move(r, c);
-                                    if (!movelist.contains(valid)) {
-                                        movelist.add(valid);
-                                    }
-                                    /*whatever makes this a valid move*/
-                                } else {
-                                    /* not a valid move*/
-                                }
-                            }
-                        }
-                    }//Checks all of the direction wheel
-                }
-            }
-        }
-    }//Finds the valid moves
+	public void BoardDraw() {
+		for (int x = 0; x < 10; x++) {
+			System.out.print("C |");
+			for (int y = 0; y < 10; y++) {
+				switch (board[x][y]) {
+				case Game.BLACK:
+					System.out.print("B");
+					break;
+				case Game.BORDER:
+					System.out.print("X");
+					break;
+				case Game.EMPTY:
+					System.out.print("0");
+					break;
+				case Game.WHITE:
+					System.out.print("W");
+					break;
+				}
+				System.out.print("|");
+			}
+			System.out.println();
+		} // Print the initial board state
+		System.out.println();
+	}// constructor
 
-    /**
-     *
-     * @param player -- player for which moves are generated
-     * @return a list of moves for player
-     */
-    public ArrayList<Move> generateMoves(int player) {
-        ArrayList<Move> moveList = new ArrayList<>();
-        FindValidMoves(player, moveList);
-        return moveList;
-    }
+	public Board(Board board) {
+		for (int r = 1; r <= 9; r++) {
+			for (int c = 1; c <= 9; c++) {
+				this.board[r][c] = board.board[r][c];
+			}
+		}
+	}// AlphaBeta constructor
 
-    public boolean testMove(int player, Move move) {
-        ArrayList<Move> moveList = generateMoves(player);
-        return (moveList.indexOf(move) != -1);
-    }
+	public void FindValidMoves(int color, ArrayList<Move> movelist) {
+		int r;
+		int c;
+		int opp = -color;
+		for (r = 1; r <= 9; r++) {
+			for (c = 1; c <= 9; c++) {
+				if (board[r][c] == Game.EMPTY) {
+					// System.out.println("found empty space " + r + " " + c);
+					for (int dR = -1; dR <= 1; dR++) {
+						for (int dC = -1; dC <= 1; dC++) {
+							if (board[r + dR][c + dC] == opp) {
+								int OppPiece = 1;// Possibly 2
+								// System.out.println("test " + r + " " + c + " " + dR + " " + dC);
+								// System.out.println((r+OppPiece*dR) + " " + (c+OppPiece*dC));
+								while (board[r + OppPiece * dR][c + OppPiece * dC] == opp) {
+									// board[r][c] = board[r - dR][c - dC];
+									OppPiece++;
+									// System.out.println("looping");
+								}
+								// System.out.println((r+OppPiece*dR) + " " + (c+OppPiece*dC));
+								if (board[r + OppPiece * dR][c + OppPiece * dC] == color) {
+									// System.out.println("add to valid move");
+									Move valid = new Move(r, c);
+									if (!movelist.contains(valid)) {
+										movelist.add(valid);
+									}
+									/* whatever makes this a valid move */
+								} else {
+									/* not a valid move */
+								}
+							}
+						}
+					} // Checks all of the direction wheel
+				}
+			}
+		}
+	}// Finds the valid moves
 
-    /**
-     * applies given move on board for given player
-     *
-     * @param player
-     * @param move
-     */
-    public boolean applyMove(int player, Move move) {
-        ArrayList<Move> moveList = generateMoves(player);
-        if (moveList.indexOf(move) != -1) {
-            int opp = -player;
-            board[move.getR()][move.getC()] = player;
-            for (int dR = -1; dR <= 1; dR++) {
-                for (int dC = -1; dC <= 1; dC++) {
-                    if (board[move.getR() + dR][move.getC() + dC] == opp) {
-                        moveList.removeAll(moveList);
-                        moveList.add(new Move(move.getR() + dR, move.getC() + dC));
-                        int OppPiece = 1;//Possibly 2
-                        //System.out.println("test " + r + " " + c + " " + dR + " " + dC);
-                        //System.out.println((r+OppPiece*dR) + " " + (c+OppPiece*dC));
-                        while (board[move.getR() + OppPiece * dR][move.getC() + OppPiece * dC] == opp) {
-                            //board[move.getR()][move.getC()] = board[move.getR() - dR][move.getC() - dC];
-                            OppPiece++;
+	/**
+	 *
+	 * @param player
+	 *            -- player for which moves are generated
+	 * @return a list of moves for player
+	 */
+	public ArrayList<Move> generateMoves(int player) {
+		ArrayList<Move> moveList = new ArrayList<>();
+		FindValidMoves(player, moveList);
+		return moveList;
+	}
 
-                            moveList.add(new Move(move.getR() + (dR * OppPiece), move.getC() + (dC * OppPiece)));
-                            //System.out.println("looping");
-                        }
-                        //System.out.println((r+OppPiece*dR) + " " + (c+OppPiece*dC));
-                        if (board[move.getR() + OppPiece * dR][move.getC() + OppPiece * dC] == player) {
-                            //System.out.println("add to valid move");
-                            while (!moveList.isEmpty()) {
-                                Move flippedTile = moveList.remove(0);
-                                board[flippedTile.getR()][flippedTile.getC()] = player;
-                            }
-                            /*whatever makes this a valid move*/
-                        } else {
-                            /* not a valid move*/
-                        }
-                    }
-                }
-            }
-            return true;
-        }else{
-            return false;
-        }
-    }//iterate through, count the pieces
+	public boolean testMove(int player, Move move) {
+		ArrayList<Move> moveList = generateMoves(player);
+		return (moveList.indexOf(move) != -1);
+	}
 
-    /**
-     *
-     * @return true if game is over false otherwise
-     */
-    public boolean gameOver() {
-        ArrayList<Move> moveListB = generateMoves(-1);
-        ArrayList<Move> moveListW = generateMoves(1);
-        return moveListB.isEmpty() && moveListW.isEmpty();
-    }
+	/**
+	 * applies given move on board for given player
+	 *
+	 * @param player
+	 * @param move
+	 */
+	public boolean applyMove(int player, Move move) {
+		ArrayList<Move> moveList = generateMoves(player);
+		if (moveList.indexOf(move) != -1) {
+			int opp = -player;
+			board[move.getR()][move.getC()] = player;
+			for (int dR = -1; dR <= 1; dR++) {
+				for (int dC = -1; dC <= 1; dC++) {
+					if (board[move.getR() + dR][move.getC() + dC] == opp) {
+						moveList.removeAll(moveList);
+						moveList.add(new Move(move.getR() + dR, move.getC() + dC));
+						int OppPiece = 1;// Possibly 2
+						while (board[move.getR() + OppPiece * dR][move.getC() + OppPiece * dC] == opp) {
+							OppPiece++;
 
-    public int countBlack(){
-        int blackPiece = 0;
-        for(int r = 1; r <= 9; r++){
-            for(int c = 1; c <= 9; c++){
-                if(board[r][c] == -1){
-                    blackPiece++;
-                }
-            }
-        }
-        return blackPiece;
-    }
-    
-    public int moveVal(int row, int column){
-        int value[][] =  {{100, 1, 20, 20, 20, 20, 1, 100},
-                            {1, 1, 5, 5, 5, 5, 1, 1}, 
-                            {20, 5, 2, 2, 2, 2, 5, 20}, 
-                            {20, 5, 2, 0, 0, 2, 5, 20}, 
-                            {20, 5, 2, 0, 0, 2, 5, 20}, 
-                            {20, 5, 2, 2, 2, 2, 5, 20}, 
-                            {1, 1, 5, 5, 5, 5, 1, 1}, 
-                            {100, 1, 20, 20, 20, 20, 1, 100}};
-        return value[row][column];
-    }//Value of positions on the board
-    
-    public int evaluate(){
-        ArrayList<Move> moveList = new ArrayList<>();
-        int Bestscore = 0;
-        int Bestcolumn = 0;
-        int Bestrow = 0;
-        for(int i=0; i<moveList.size(); i++){
-            Move position = moveList.get(i);
-            int r = position.r;
-            int c = position.c;
-            if(moveVal(r, c)> Bestscore){
-                Bestcolumn = c;
-                Bestrow = r;
-            }
-        }
-        return this.moveVal(Bestrow, Bestcolumn);
-    }
-    
-}//Board class
+							moveList.add(new Move(move.getR() + (dR * OppPiece), move.getC() + (dC * OppPiece)));
+						}
+						if (board[move.getR() + OppPiece * dR][move.getC() + OppPiece * dC] == player) {
+							while (!moveList.isEmpty()) {
+								Move flippedTile = moveList.remove(0);
+								board[flippedTile.getR()][flippedTile.getC()] = player;
+							}
+							/* whatever makes this a valid move */
+						} else {
+							/* not a valid move */
+						}
+					}
+				}
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}// iterate through, count the pieces
+
+	/**
+	 *
+	 * @return true if game is over false otherwise
+	 */
+	public boolean gameOver() {
+		ArrayList<Move> moveListB = generateMoves(-1);
+		ArrayList<Move> moveListW = generateMoves(1);
+		return moveListB.isEmpty() && moveListW.isEmpty();
+	}
+
+	public int countBlack() {
+		int blackPiece = 0;
+		for (int r = 1; r <= 9; r++) {
+			for (int c = 1; c <= 9; c++) {
+				if (board[r][c] == -1) {
+					blackPiece++;
+				}
+			}
+		}
+		return blackPiece;
+	}
+
+	public int moveVal(int row, int column) {
+		int value[][] = { { 100, 1, 20, 20, 20, 20, 1, 100 }, { 1, 1, 5, 5, 5, 5, 1, 1 }, { 20, 5, 2, 2, 2, 2, 5, 20 },
+				{ 20, 5, 2, 0, 0, 2, 5, 20 }, { 20, 5, 2, 0, 0, 2, 5, 20 }, { 20, 5, 2, 2, 2, 2, 5, 20 },
+				{ 1, 1, 5, 5, 5, 5, 1, 1 }, { 100, 1, 20, 20, 20, 20, 1, 100 } };
+		return value[row][column];
+	}// Value of positions on the board
+
+	public int evaluate() {
+		ArrayList<Move> moveList = new ArrayList<>();
+		int Bestscore = 0;
+		int Bestcolumn = 0;
+		int Bestrow = 0;
+		for (int i = 0; i < moveList.size(); i++) {
+			Move position = moveList.get(i);
+			int r = position.r;
+			int c = position.c;
+			if (moveVal(r, c) > Bestscore) {
+				Bestcolumn = c;
+				Bestrow = r;
+			}
+		}
+		return this.moveVal(Bestrow, Bestcolumn);
+	}
+
+}// Board class
--- Othello/src/mcfothelloai/McfOthelloAI.java
+++ Othello/src/mcfothelloai/McfOthelloAI.java
@@ -6,74 +6,66 @@
 package mcfothelloai;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class McfOthelloAI {
 
-    public int color;
-    public char cColor;
-    public McfOthelloAI(int color){
-        this.color = color;
-        if(color == Game.BLACK){
-            cColor = 'B';
-            System.out.println("R B");
-        }else if(color == Game.WHITE){
-            cColor = 'W';
-            System.out.println("R W");
-        }
-    }
-    
-//    public Move findMove(Board board){
-//        ArrayList<Move> test = board.generateMoves(color);
-//        if(test.isEmpty()){
-//            return new Move(-1,-1);
-//        }
-//        Random RNG = new Random();
-//        int rMove = RNG.nextInt(test.size());
-//        return test.get(rMove);
-//    }
-    
-    public Move findMove(Board currentBoard, int ply, int player, double alpha, double beta, int maxDepth){
-        if (ply >= maxDepth){
-            Move returnMove = new Move(0, 0);
-            returnMove.value = currentBoard.evaluate();
-            return returnMove;
-        }    
-        else {
-            /*
-                1. generate Moves for player
-                2. if MoveList is empty, add passmove to movelist
-                3. bestMove = moveList.get(0)
-                4. for each move in the move list
-                   a.) newBoard = currentBoard.applyMove(player, move)
-                   b.) tempMove = alphaBeta(newBoard, ply+1, -player, -beta, -alpha, maxDepth)
-                   c.) move.value = -tempMove.value;
-                   d.) if moveValue > alpha
-                       c0) bestMove = move
-                       c1) alpha = moveValue
-                       c2) if alpha > beta
-                             return bestMove
-               5.) return bestMove        
-            */
-            ArrayList<Move> moves = currentBoard.generateMoves(player);
-            if (moves.isEmpty()) moves.add(new Move(0, 0)); //add pass move if empty
-            Move bestMove = moves.get(0);
-            for (Move move : moves){
-                Board newBoard = new Board(currentBoard);
-                newBoard.applyMove(player, move);
-                Move tempMove =  findMove(newBoard, ply+1, -player, -beta, -alpha, maxDepth);
-                move.value = -tempMove.value;
-                if (move.value > alpha){
-                    bestMove = move;
-                    alpha = move.value;
-                    if (alpha > beta)
-                        return bestMove;
-                }
-            }
-            return bestMove;
-        }
-        
-    }
-    
-    
+	public int color;
+	public char cColor;
+
+	public McfOthelloAI(int color) {
+		this.color = color;
+		if (color == Game.BLACK) {
+			cColor = 'B';
+			System.out.println("R B");
+		} else if (color == Game.WHITE) {
+			cColor = 'W';
+			System.out.println("R W");
+		}
+	}
+
+	// public Move findMove(Board board){
+	// ArrayList<Move> test = board.generateMoves(color);
+	// if(test.isEmpty()){
+	// return new Move(-1,-1);
+	// }
+	// Random RNG = new Random();
+	// int rMove = RNG.nextInt(test.size());
+	// return test.get(rMove);
+	// }
+
+	public Move findMove(Board currentBoard, int ply, int player, double alpha, double beta, int maxDepth) {
+		if (ply >= maxDepth) {
+			Move returnMove = new Move(0, 0);
+			returnMove.value = currentBoard.evaluate();
+			return returnMove;
+		} else {
+			/*
+			 * 1. generate Moves for player 2. if MoveList is empty, add passmove to
+			 * movelist 3. bestMove = moveList.get(0) 4. for each move in the move list a.)
+			 * newBoard = currentBoard.applyMove(player, move) b.) tempMove =
+			 * alphaBeta(newBoard, ply+1, -player, -beta, -alpha, maxDepth) c.) move.value =
+			 * -tempMove.value; d.) if moveValue > alpha c0) bestMove = move c1) alpha =
+			 * moveValue c2) if alpha > beta return bestMove 5.) return bestMove
+			 */
+			ArrayList<Move> moves = currentBoard.generateMoves(player);
+			if (moves.isEmpty())
+				moves.add(new Move(0, 0)); // add pass move if empty
+			Move bestMove = moves.get(0);
+			for (Move move : moves) {
+				Board newBoard = new Board(currentBoard);
+				newBoard.applyMove(player, move);
+				Move tempMove = findMove(newBoard, ply + 1, -player, -beta, -alpha, maxDepth);
+				move.value = -tempMove.value;
+				if (move.value > alpha) {
+					bestMove = move;
+					alpha = move.value;
+					if (alpha > beta)
+						return bestMove;
+				}
+			}
+			return bestMove;
+		}
+
+	}
+
 }
